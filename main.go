4bc10cbd18905ffdcd6c68457c0ec238a48be6ba
Alvin Choong <alvin.choong92@gmail.com>
2024-03-30 23:05:42 +0800

    tenants post and get api

--- main.go
+++ main.go
@@ -59,19 +59,62 @@ func Handler(ctx context.Context, pool *db.Pool, slugDBCfg map[string]string) *c
 
 	r.Use(TenantSlugMiddleware)
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	r.Post("/api/tenants", func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		slug := slugFromContext(ctx)
 		db := pickDBPool(pool, slugDBCfg, slug)
 
-		var dbTenant string
-		err := db.QueryRow(ctx, "SELECT current_setting('app.current_tenant')").Scan(&dbTenant)
+		var tenant Tenant
+		err := json.NewDecoder(r.Body).Decode(&tenant)
+		if err != nil {
+			http.Error(w, "json.Decode failed", http.StatusBadRequest)
+			return
+		}
+
+		err = db.QueryRow(ctx, `
+		INSERT INTO tenants (slug, description) 
+		VALUES ($1, $2) RETURNING *`, tenant.Slug, tenant.Description).
+			Scan(&tenant.Slug, &tenant.Description, &tenant.CreatedAt, &tenant.UpdatedAt)
 		if err != nil {
 			http.Error(w, fmt.Sprintf("db.QueryRow failed: %+v", err), http.StatusInternalServerError)
 			return
 		}
 
-		fmt.Fprintf(w, "ctxslug: %s\ndb.tenant: %s\n", slug, dbTenant)
+		b, err := json.Marshal(tenant)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("json.Marshal failed: %+v", err), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(b)
+	})
+
+	r.Get("/api/tenants", func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		slug := slugFromContext(ctx)
+		db := pickDBPool(pool, slugDBCfg, slug)
+
+		rows, err := db.Query(ctx, `SELECT * FROM tenants`)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("db.Query failed: %+v", err), http.StatusInternalServerError)
+			return
+		}
+
+		tenants, err := pgx.CollectRows(rows, pgx.RowToStructByName[Tenant])
+		if err != nil {
+			http.Error(w, fmt.Sprintf("pgx.CollectRows failed: %+v", err), http.StatusInternalServerError)
+			return
+		}
+
+		b, err := json.Marshal(tenants)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("json.Marshal failed: %+v", err), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(b)
 	})
 
 	r.Get("/users/add", func(w http.ResponseWriter, r *http.Request) {
@@ -154,6 +197,13 @@ func Handler(ctx context.Context, pool *db.Pool, slugDBCfg map[string]string) *c
 	})
 
 	return r
+}
+
+type Tenant struct {
+	Slug        string    `json:"slug"`
+	Description *string   `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
 
 type User struct {
